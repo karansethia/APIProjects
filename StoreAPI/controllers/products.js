569a7fa5db95476dddde51f9cee17eb7da02f8ea
Karan <karansethia24@gmail.com>
2023-12-03 01:19:22 +0530

    API for notes application

--- StoreAPI/controllers/products.js
+++ StoreAPI/controllers/products.js
@@ -1,9 +1,28 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async(req,res) => {
   // throw Error('testing async errors')
-  res.status(200).json({message: " testing"})
+  const products = await Product.find({featured: true})
+
+  res.status(200).json({Products: products, nbHits: products.length})
 }
 const getAllProducts = async(req,res) => {
-  res.status(200).json({message: "All Products"})
+  // const featured = req.query.featured;
+
+  const { featured,company } = req.query;
+  const queryObj = {};
+  if(featured){
+    queryObj.featured = featured==='true' ? true : false;
+  }
+  if(company){
+    queryObj.company = company;
+  }
+
+//if request.query has all wrong keys then essentially queryObj is empty
+//and if queryObj is empty we are essentially passing an empty object to MOngoose which will return us all the data values
+  console.log(queryObj);
+  const products = await Product.find(queryObj)
+  res.status(200).json({nbHits: products.length,products: products})
 }
 module.exports = {
   getAllProducts,
--- StoreAPI/populate.js
+++ StoreAPI/populate.js
@@ -1,1 +1,20 @@
 require('dotenv').config();
+
+const connectDB = require('./db/connect');
+const Product = require('./models/product');
+const data = require('./products.json');
+
+const start = async() => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    await Product.deleteMany();
+    await Product.create(data);
+    console.log('succesfuly populated');
+    process.exit(0)  //exiting the process at 0 means everythung went well
+  } catch (error) {
+    console.log(error);
+    process.exit(1); // exiting the process at 1 means something went wrong
+  }
+}
+
+start()
