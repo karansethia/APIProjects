23a57d508d14c0138bbff0e5318004a3f78e2d54
Karan <karansethia24@gmail.com>
2023-12-03 01:31:34 +0530

    Update: Basic app structure

--- NotesAPI/index.js
+++ NotesAPI/index.js
@@ -1,4 +1,27 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
-app.listen(3100);
+require('dotenv').config();
+
+
+app.use(cors({
+  origin: '*'
+}));
+
+app.use(express.json());
+
+//todo routes
+// -> api/v1/login 
+// -> api/v1/register
+// -> api/v1/refresh
+// -> api/v1/notes 
+// -> api/v1/email 
+
+const PORT = processe.env.PORT || 3100
+
+const startServer = (port) => {
+  app.listen(port);
+}
+
+startServer(PORT)
